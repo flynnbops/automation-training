29813eed3001cf4d7f5b81403e71b8e694eb8346
Aaron Flynn <16778356+flynnbops@users.noreply.github.com>
2021-12-30 16:35:27 +0000

    Updated prototype kit to version 12.0.0

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
-# automation-training
-This is a training site, built using the GOV.uk prototype kit.
+# Automation training
+This is a training site, built using the [GOV.uk prototype kit](https://govuk-prototype-kit.herokuapp.com/docs).
 
-It was built as an accessible site, where people can practice writing automated tests.
-It does not send any information anywhere.
-Other that having some pages linked together with different types of input it doesn't do anything.
+It was originally  built so people can practice writing automated tests against something that is "gov.uk like". It is not an accurate representation of a government service. But, it does use components and patterns from the [design system](https://design-system.service.gov.uk/get-started/).
+
+The current version is hosted on heroku at https://automation-training.herokuapp.com/.
+
+Information between pages is stored as browser session data. No information is "sent anywhere" when the mock application is complete. This is just pages connected together with no validation of input.
+
+# Installation and updating
+Are both covered in the prototype docs. See [install instructions](https://govuk-prototype-kit.herokuapp.com/docs/install/developer-install-instructions) and [update instructions](https://govuk-prototype-kit.herokuapp.com/docs/updating-the-kit) for more details.
+
+# Local development
+
+1. Navigate to the project root e.g. `automation-training`
+2. Install dependencies `npm install`
+3. Start the app `npm start`
+4. Accept / reject `the anonymous usage data` prompt
+5. The default address of the app is `localhost:3000`
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Core dependencies
 const fs = require('fs')
 const path = require('path')
+const url = require('url')
 
 // NPM dependencies
 const bodyParser = require('body-parser')
@@ -9,7 +10,6 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 const sessionInCookie = require('client-sessions')
 const sessionInMemory = require('express-session')
-const cookieParser = require('cookie-parser')
 
 // Run before other code to make sure variables from .env are available
 dotenv.config()
@@ -45,15 +45,10 @@ if (useV6) {
   v6App = express()
 }
 
-// Set cookies for use in cookie banner.
-app.use(cookieParser())
-documentationApp.use(cookieParser())
-app.use(utils.handleCookies(app))
-documentationApp.use(utils.handleCookies(documentationApp))
-
 // Set up configuration variables
 var releaseVersion = packageJson.version
-var env = (process.env.NODE_ENV || 'development').toLowerCase()
+var glitchEnv = (process.env.PROJECT_REMIX_CHAIN) ? 'production' : false // glitch.com
+var env = (process.env.NODE_ENV || glitchEnv || 'development').toLowerCase()
 var useAutoStoreData = process.env.USE_AUTO_STORE_DATA || config.useAutoStoreData
 var useCookieSessionStore = process.env.USE_COOKIE_SESSION_STORE || config.useCookieSessionStore
 var useHttps = process.env.USE_HTTPS || config.useHttps
@@ -108,7 +103,7 @@ app.set('view engine', 'html')
 // Middleware to serve static assets
 app.use('/public', express.static(path.join(__dirname, '/public')))
 
-// Serve govuk-frontend in from node_modules (so not to break pre-extenstions prototype kits)
+// Serve govuk-frontend in from node_modules (so not to break pre-extensions prototype kits)
 app.use('/node_modules/govuk-frontend', express.static(path.join(__dirname, '/node_modules/govuk-frontend')))
 
 // Set up documentation app
@@ -161,7 +156,6 @@ if (useV6) {
 app.locals.asset_path = '/public/'
 app.locals.useAutoStoreData = (useAutoStoreData === 'true')
 app.locals.useCookieSessionStore = (useCookieSessionStore === 'true')
-app.locals.cookieText = config.cookieText
 app.locals.promoMode = promoMode
 app.locals.releaseVersion = 'v' + releaseVersion
 app.locals.serviceName = config.serviceName
@@ -170,7 +164,7 @@ app.locals.extensionConfig = extensions.getAppConfig()
 
 // Session uses service name to avoid clashes with other prototypes
 const sessionName = 'govuk-prototype-kit-' + (Buffer.from(config.serviceName, 'utf8')).toString('hex')
-let sessionOptions = {
+const sessionOptions = {
   secret: sessionName,
   cookie: {
     maxAge: 1000 * 60 * 60 * 4, // 4 hours
@@ -214,8 +208,6 @@ app.post('/prototype-admin/clear-data', function (req, res) {
 // Redirect root to /docs when in promo mode.
 if (promoMode === 'true') {
   console.log('Prototype Kit running in promo mode')
-
-  app.locals.cookieText = 'GOV.UK uses cookies to make the site simpler. <a href="/docs/cookies">Find out more about cookies</a>'
 
   app.get('/', function (req, res) {
     res.redirect('/docs')
@@ -309,7 +301,11 @@ if (useV6) {
 
 // Redirect all POSTs to GETs - this allows users to use POST for autoStoreData
 app.post(/^\/([^.]+)$/, function (req, res) {
-  res.redirect('/' + req.params[0])
+  res.redirect(url.format({
+    pathname: '/' + req.params[0],
+    query: req.query
+  })
+  )
 })
 
 // Catch 404 and forward to error handler
